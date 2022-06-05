47ee83d0a6dc470b3f2e010e76653ed5c64ce877
Kemmei Hamada <sageglorieux2170@hotmail.com>
2022-06-05 13:07:21 -0700

    fix: move back styles to indext.tsx  Changes to be committed: 	deleted:    src/pages/index.styles.ts 	modified:   src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import useStyles from './index.styles';
+import { makeStyles, Theme } from '@material-ui/core/styles';
 
 import { useAppSelector } from '../app/hooks';
 import ButtonGroup from '../components/ButtonGroup';
@@ -9,6 +9,58 @@ import Card from '../components/Card';
 import SwitchWallpaper from '../components/SwitchWallpaper';
 import Credits from '../components/Credits';
 import Footer from '../components/Footer';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    flexGrow: 1,
+    height: '100vh',
+    background: '#222',
+    backgroundImage: 'linear-gradient(#222,#777,#222)',
+  },
+  titleApp: {
+    fontFamily: 'Lobster',
+    color: 'white',
+    textShadow: '2px 2px #19857b',
+    padding: theme.spacing(4, 0),
+  },
+  container: {
+    height: '100vh',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    // backgroundSize: 'fit',
+    [theme.breakpoints.down('sm')]: {
+      '@media (orientation: landscape)': {
+        backgroundSize: 'auto 100vw',
+        height: '100vw',
+      },
+      '@media (orientation: portrait)': {
+        backgroundSize: 'auto 100vh',
+        height: '100vh',
+      },
+    },
+  },
+  wallpaper1: {
+    backgroundImage:
+      'linear-gradient(to bottom, rgba(0,20,0,1.0), rgba(0,50,0,0.0), rgba(0,20,1.0)), url("/images/flowers1.jpg")',
+  },
+  wallpaper2: {
+    backgroundImage:
+      'linear-gradient(to bottom, rgba(0,0,0,1.0), rgba(0,20,0,0.0), rgba(0,0,0,1.0)), url("/images/bamboo1.jpg")',
+  },
+  cardContainer: {
+    marginTop: '1.0rem',
+  },
+  switchContainer: {
+    marginTop: '1.5rem',
+    backgroundColor: 'rgba(180,180,255,0.8)',
+    padding: '0 0.5rem',
+    borderRadius: '0.4rem',
+  },
+
+  footerContainer: {
+    marginTop: theme.spacing(5),
+  },
+}));
 
 const Home = () => {
   const classes = useStyles();
