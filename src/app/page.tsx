88d7cfbbe9205cb01c512a32aa00d315c2ed0427
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2024-11-08 15:54:24 -0800

    delete Credits component; move wrapper style to container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
-// "use client";
-
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
 
 import Card from "../components/Card";
-import Credits from "../components/Credits";
 import Footer from "../components/Footer";
 import { numOfWallpapers, wallpapers } from "../assets/wallpapers";
 import BackgroundImage from "../components/backgroundImage";
-import { Wrapper } from "./styles";
 
 const imageLoader = () => {
   const pickedId = Math.floor(Math.random() * numOfWallpapers);
@@ -22,7 +17,17 @@ const Home = () => {
   const image = imageLoader();
 
   return (
-    <Container maxWidth="lg">
+    <Container
+      maxWidth="lg"
+      sx={{
+        position: "relative",
+        width: "100vw",
+        height: "100vh",
+        backgroundImage:
+          "radial-gradient( rgba(233, 233, 233, 1),rgba(114, 114, 114, 1))",
+        zIndex: 10,
+      }}
+    >
       <BackgroundImage img={image} />
       <Grid
         container
@@ -38,14 +43,14 @@ const Home = () => {
             fontWeight: "bold",
             color: "white",
             textShadow: "2px 2px #19857b",
-            padding: "1rem",
+            marginTop: "2rem",
+            marginBottom: "2rem",
           }}
         >
           Advice App
         </Typography>
 
         <Card />
-        {/* <Credits /> */}
         <Footer />
       </Grid>
     </Container>
