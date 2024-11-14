8c0607448852d7b96d13d7280616e2aace3d691e
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2024-11-14 00:40:50 -0800

    modify loading indicator

--- src/components/LoadingIndicator.tsx
+++ src/components/LoadingIndicator.tsx
@@ -1,60 +1,74 @@
 import { keyframes, styled } from "@mui/material/styles";
 
+const maxScale = 1.5;
+const minOpacity = 0.2;
+
 const pulseL = keyframes`
   0% {
      opacity: 1.0;
+     scale: ${maxScale};
   }
   50% {
-     opacity: 0.3;
+     opacity: ${minOpacity};
+     scale: 1;
   }
   100% {
-     opacity: 0.3;
+     opacity: ${minOpacity};
+     scale: 1;
   }
 `;
 
 const pulseM = keyframes`
   0% {
-     opacity: 0.3;
+     opacity: ${minOpacity};
+     scale: 1;
   }
   50% {
      opacity: 1.0;
+     scale: ${maxScale};
   }
   100% {
-     opacity: 0.3;
+     opacity: ${minOpacity};
+     scale: 1;
   }
 `;
 
 const pulseR = keyframes`
   0% {
-     opacity: 0.3;
+     opacity: ${minOpacity};
+     scale: 1;
   }
   50% {
-     opacity: 0.3;
+     opacity: ${minOpacity};
+     scale: 1;
   }
   100% {
      opacity: 1.0;
+     scale: ${maxScale};
   }
 `;
 
 const Dot = styled("div")({
   display: "inline-block",
-  margin: "0 0.5rem",
+  margin: "0 0.75rem",
   width: "1.0rem",
   height: "1.0rem",
   borderRadius: "50%",
   backgroundColor: "purple",
 });
 
+const period = 0.75;
+
 const DotIndicatorLeft = styled(Dot)({
-  animation: `${pulseL} 0.3s infinite ease-in-out`,
+  animation: `${pulseL} ${period}s infinite ease-in-out`,
 });
 
 const DotIndicatorMiddle = styled(Dot)({
-  animation: `${pulseM} 0.3s infinite ease-in-out`,
+  animation: `${pulseM} ${period}s infinite ease-in-out`,
 });
 
 const DotIndicatorRight = styled(Dot)({
-  animation: `${pulseR} 0.3s infinite ease-in-out`,
+  animation: `${pulseR} ${period}s infinite ease-in-out`,
 });
 
 const LoadingIndicator = () => {
