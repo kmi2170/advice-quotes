b108cae90b3d8d6ecc6604300a1e0ff586fb00dc
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2024-11-20 22:41:49 -0800

    fix title

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -9,11 +9,19 @@ const Title = ({ apiName }: { apiName: ApiNameType }) => {
         return "Advice";
       case API_NAMES.QUOTES:
         return "Quote";
+      case API_NAMES.LIFE_HACKS:
+        return "Life Hacks";
+      case API_NAMES.FORTUNE_COOKIE:
+        return "Fortune Cookie";
+      case API_NAMES.USELESS_FACTS:
+        return "Useless Facts";
+      case API_NAMES.JESTER:
+        return "Joke";
     }
   };
 
   return (
-    <Typography variant="h2" sx={{ fontWeight: "bold" }}>
+    <Typography variant="h3" sx={{ fontStyle: "italic", fontWeight: "bold" }}>
       {renderTitle(apiName)}
     </Typography>
   );
