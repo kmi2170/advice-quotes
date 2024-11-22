8cd56200abbd6f33f3987b92c762e0748e5a8012
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2024-11-22 01:43:22 -0800

    fix api key

--- src/api/lib/fetchQuote.ts
+++ src/api/lib/fetchQuote.ts
@@ -6,8 +6,7 @@ const options: AxiosRequestConfig = {
   url: "https://quotes15.p.rapidapi.com/quotes/random/?language_code=en",
   headers: {
     "x-rapidapi-host": process.env.NEXT_X_RAPID_API_HOST,
-    "x-rapidapi-key": (process.env.NEXT_X_RAPID_API_KEY =
-      "f9b86bb856msh2b5b28e7656e0b5p11359djsn47d89d6bf19f"),
+    "x-rapidapi-key": process.env.NEXT_X_RAPID_API_KEY,
   },
 };
 
